ec6eb67b3e961aab322c58cc6711408d4dec850b
Zyrus R. Alvez <zyrusalvez13@gmail.com>
2024-07-09 17:24:01 +0800

    done about

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -35,6 +35,178 @@ export default function About() {
           fans and new shopping enthusiasts. Download the Shopee app now and
           visit the Shopee website too!
         </p>
+
+        <h1 className={style.title}>
+          Experience a Shopping Spree of a Lifetime on Shopee
+        </h1>
+        <p className={style.p}>
+          Join Shopee to find everything you need at the best prices. Doing your
+          Shopee online shopping at the Philippines’ best marketplace cannot get
+          any easier. Shopee is a social marketplace where you can enjoy instant
+          and personalized updates from your friends and favorite community
+          members. If you spot great products or deals while you’re doing your
+          Shopee online shopping, Shopee enables you to share these deals with
+          your friends via a simple tap.
+          <div className={style.space}></div>
+          Buy the products you want in a worry-free manner. Refer to shop
+          ratings and reviews to find trusted sellers easily. We value our safe
+          online marketplace! With Shopee Guarantee, you can get your money back
+          if you did not get what you ordered. Take note of your Shopee
+          Guarantee period so that you can choose to extend it if the seller has
+          not shipped your product or if you want to request a{" "}
+          <span className={style.underline}>return or refund</span>. Shopee
+          Guarantee is here to ensure that you have a fun shopping experience.
+          You can rest easy knowing that Shopee Guarantee has your back!
+          <div className={style.space}></div>
+          Not sure what to buy? Our new hashtags allow you to stay up-to-date
+          with trending products. Browse effortlessly through our product
+          categories including <span className={style.underline}>
+            jewelry
+          </span>{" "}
+          from <span className={style.underline}>women's accessories </span>{" "}
+          like <span className={style.underline}>necklaces</span> that are
+          perfect gifts for moms.{" "}
+          <span className={style.underline}>Mobiles</span> for your dad that you
+          can find in{" "}
+          <span className={style.underline}>mobiles and gadgets</span>. Get
+          fan-favorite mobile phones like{" "}
+          <span className={style.underline}>Poco F3</span>,{" "}
+          <span className={style.underline}>Poco X3 Pro</span>,{" "}
+          <span className={style.underline}>Redmi Note 11</span>, and{" "}
+          <span className={style.underline}>Realme C35</span>. Then amp up the
+          music in your phone with{" "}
+          <span className={style.underline}>volume master</span>! Look for the
+          best <span className={style.underline}>stroller</span> for your
+          newborn, and surprise the kids with new{" "}
+          <span className={style.underline}>toys, games & collectibles</span>.
+          Buy your sister the most volumizing or lengthening{" "}
+          <span className={style.underline}>mascara</span> or{" "}
+          <span className={style.underline}>hair tie</span>. You can never go
+          wrong with buying a cap for your brother. Purchase{" "}
+          <span className={style.underline}>flowers and plants</span> that your
+          aunt and uncle or grandparents will surely love! Start your fitness
+          journey with{" "}
+          <span className={style.underline}>
+            exercise and fitness equipment
+          </span>{" "}
+          and get health and personal care items too like{" "}
+          <span className={style.underline}>Gloxi</span> and more! Show love to
+          your little pet by giving it the best{" "}
+          <span className={style.underline}>pet essentials</span> from Shopee’s
+          wide array of
+          <span className={style.underline}>pet care</span> items and many more!
+          Spruce up your home with products inspired by{" "}
+          <span className={style.underline}>Ikea Philippines</span> and other
+          amazing <span className={style.underline}>home and living</span>{" "}
+          brands. Find trending <span className={style.underline}>Youtube</span>{" "}
+          products on Shopee Philippines too!
+          <div className={style.space}></div>
+          <div className={style.space}></div>
+          Eager to find out what's trending? Shopee has all the latest trending
+          products, including <span className={style.underline}>
+            Piso Wifi
+          </span>{" "}
+          which is all the rage nowadays! Find out what the buzz is all about
+          and discover the wonders of Piso Wifi. While shopping, use our smart
+          search or look through a range of personalized recommendations to find
+          the perfect buy. Plus, enjoy additional benefits such as free shipping
+          for selected products. Start shopping on Shopee now!
+          <div className={style.space}></div>
+          So what are you waiting for? Open your Shopee app or visit the Shopee
+          website and get on with your shopping!
+          <h1 className={style.title}>
+            Enjoy Special Deals, Sales, Promos, and Discounts on Shopee
+            Philippines
+          </h1>
+          <p className={style.p}>
+            Doing your Shopee online shopping is not only easy and safe, but
+            it’s also tons of fun. Enjoy big sales like the 9.9 Super Shopping
+            Day and the 11.11 Christmas Sale to score the biggest and best
+            discounts and special prices on your favorite products. Treat
+            yourself during payday with Shopee’s Payday Sale on the 15th and
+            30th of every month. With special promotions from shops such as
+            Shopee’s free shipping vouchers, deals, and flash sales, to weekly
+            offers - you’re sure to become a true Shopeeholic. Catch the Shopee
+            fever with regular deals on your favorite categories only on Shopee
+            Philippines! Download the app on your mobile phone now. If you’re
+            new to the Shopee fam, you get the chance to enjoy Shopee New User
+            vouchers too! Shopee New User vouchers are the perfect way to
+            welcome you into your favorite shopping destination.
+            <div className={style.space}></div>
+            This is your sign to create a Shopee account and start shopping on
+            the Shopee website or Shopee app! Don’t miss out on the amazing
+            sales with affordable prices that are waiting for you!
+          </p>
+          <h1 className={style.title}>Get Free Shipping on Shopee</h1>
+          <p className={style.p}>
+            Aside from Shopee New User vouchers, you can also enjoy Shopee’s
+            free shipping vouchers today! For buyers, shop to your heart’s
+            content and enjoy lower prices for your purchases with the help of
+            Shopee’s free shipping vouchers. Make sure to keep an eye out for
+            Shopee’s free shipping vouchers so you can say goodbye to added
+            delivery fees! You don’t have to worry about the price going up
+            because of shipping. Selling can’t get any easier on Shopee.
+          </p>
+          <h1 className={style.title}>
+            Select Your Payment Method of Choice on Shopee
+          </h1>
+          <p className={style.p}>
+            Banks, e-wallets, ShopeePay - the choice is all up to you! If you
+            have a preferred payment method, rest assured that it’s available on
+            Shopee! Upon checking out your products, all you have to do is
+            select a payment method that is most convenient for you. Whether
+            you’re on the Shopee app or the Shopee website, you can find a wide
+            array of payment methods. Give ShopeePay a try! Activate your
+            ShopeePay account now so that you can top up your wallet and pay. On
+            top of that, you can enjoy exclusive ShopeePay vouchers including
+            Shopee’s free shipping vouchers with ShopeePay! If it’s your first
+            time placing an order, make sure to maximize your Shopee New User
+            vouchers upon checkout!
+          </p>
+          <h1 className={style.title}>Join in on the Fun with Shopee Prizes</h1>
+          <p className={style.p}>
+            To bring you even more entertainment, don’t miss out on{" "}
+            <span className={style.underline}>Shopee Prizes</span>! Play your
+            favorite games on the Shopee app like Shopee Bubble and Shopee Candy
+            to win mind-blowing prizes. To give you a hand, discover a few{" "}
+            <span className={style.underline}>tips, tricks, and hacks</span>{" "}
+            when playing Shopee’s most popular games! Take note that you can
+            only play these games on the Shopee app, not the Shopee website.
+          </p>
+          <h1 className={style.title}>Sell Online Effortlessly on Shopee</h1>
+          <p className={style.p}>
+            Shopee provides the right tools to support all our sellers on our
+            marketplace platform. List your products in less than 30 seconds!
+            Sell better and get more exposure for your products by participating
+            in our campaigns and promotions. Use{" "}
+            <span className={style.underline}>Shopee Seller Centre</span> to
+            organize your products, track orders, manage customers, and measure
+            shop performance. Share your products easily on social media
+            platforms, including Facebook, Twitter, and Instagram. You can even
+            build up your online reputation by gathering positive ratings and
+            reviews from your buyers. Shopee is completely free to download, so
+            join us and start selling today!
+          </p>
+          <h1 className={style.title}>Visit Shopee Blog</h1>
+          <p className={style.p}>
+            Want to know more about the latest must-have products, popular
+            trends, tips and tricks on various topics, and even gift guides for
+            special occasions? Proceed to{" "}
+            <span className={style.underline}>Shopee Blog</span> where you can
+            find a ton of articles on{" "}
+            <span className={style.underline}>Fashion and Beauty</span>,{" "}
+            <span className={style.underline}>Food</span>,{" "}
+            <span className={style.underline}>Parenting</span>,{" "}
+            <span className={style.underline}>Lifestyle</span>,{" "}
+            <span className={style.underline}>Tech</span>,
+            <span className={style.underline}>Shopee News</span>, and{" "}
+            <span className={style.underline}>Shopping</span>! Stay on top of
+            the latest trends and tune in on what’s coming up on Shopee with
+            Shopee Blog! From Shopee News like the latest Shopee New User
+            vouchers to the most popular trend on social media like TikTok,
+            we’ve got you covered!
+          </p>
+        </p>
       </div>
     </div>
   );
